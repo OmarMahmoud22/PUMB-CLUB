44e6eb9803db9c2063e1a549333224f56ff63780
OmarMahmou22 <omahmoudali9@gmail.com>
2026-03-09 10:22:29 +0200

    login

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const mongoose =require('mongoose')
+
+app.use(express.json())
+
+
+async function main() {
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("connected")
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+main()
+
+
+
+app.listen(port,()=>{
+    console.log(`connected with${port}`)
+})
